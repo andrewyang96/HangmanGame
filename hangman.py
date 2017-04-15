f44ccf503912dcae8e5036428f0dbfe04898d692
Andrew Yang <ayang14@illinois.edu>
2017-04-15 03:07:24 -0500

    Remove implementation

--- hangman.py
+++ hangman.py
@@ -9,43 +9,17 @@ from words import get_random_word
 
 def get_num_attempts():
     """Get user-inputted number of attempts for the game."""
-    while True:
-        num_attempts = input('How many attempts do you want? [1-25] ')
-        try:
-            num_attempts = int(num_attempts)
-            if num_attempts <= 0 or num_attempts >= 26:
-                print('{0} is not between 1 and 25'.format(num_attempts))
-            else:
-                return num_attempts
-        except ValueError:
-            print(
-                '{0} is not an integer between 1 and 25'.format(num_attempts))
+    pass  # TODO: get user-inputted number of attempts and return it
 
 
 def get_display_word(word, idxs):
     """Get the word suitable for display."""
-    if len(word) != len(idxs):
-        raise ValueError('Word length and indices length are not the same')
-    displayed_word = ''.join(
-        [letter if idxs[i] else '*' for i, letter in enumerate(word)])
-    return displayed_word.strip()
+    pass  # TODO: return word suitable for display
 
 
 def get_next_letter(remaining_letters):
     """Get the user-inputted next letter."""
-    if len(remaining_letters) == 0:
-        raise ValueError('There are no remaining letters')
-    while True:
-        next_letter = input('Choose the next letter: ').lower()
-        if len(next_letter) != 1:
-            print('{0} is not a single character'.format(next_letter))
-        elif next_letter not in ascii_lowercase:
-            print('{0} is not a letter'.format(next_letter))
-        elif next_letter not in remaining_letters:
-            print('{0} has been guessed before'.format(next_letter))
-        else:
-            remaining_letters.remove(next_letter)
-            return next_letter
+    pass  # TODO: return next letter
 
 
 def play_hangman():
@@ -53,61 +27,7 @@ def play_hangman():
 
     At the end of the game, returns if the player wants to retry.
     """
-    # Let player specify difficulty
-    print('Starting a game of Hangman...')
-    attempts_remaining = get_num_attempts()
-
-    # Randomly select a word
-    print('Selecting a word...')
-    word = get_random_word()
-    print()
-
-    # Initialize game state variables
-    idxs = [letter not in ascii_lowercase for letter in word]
-    remaining_letters = set(ascii_lowercase)
-    wrong_letters = []
-    word_solved = False
-
-    # Main game loop
-    while attempts_remaining > 0 and not word_solved:
-        # Print current game state
-        print('Word: {0}'.format(get_display_word(word, idxs)))
-        print('Attempts Remaining: {0}'.format(attempts_remaining))
-        print('Previous Guesses: {0}'.format(' '.join(wrong_letters)))
-
-        # Get player's next letter guess
-        next_letter = get_next_letter(remaining_letters)
-
-        # Check if letter guess is in word
-        if next_letter in word:
-            # Guessed correctly
-            print('{0} is in the word!'.format(next_letter))
-
-            # Reveal matching letters
-            for i in range(len(word)):
-                if word[i] == next_letter:
-                    idxs[i] = True
-        else:
-            # Guessed incorrectly
-            print('{0} is NOT in the word!'.format(next_letter))
-
-            # Decrement num of attempts left and append guess to wrong guesses
-            attempts_remaining -= 1
-            wrong_letters.append(next_letter)
-
-        # Check if word is completely solved
-        if False not in idxs:
-            word_solved = True
-        print()
-
-    # The game is over: reveal the word
-    print('The word is {0}'.format(word))
-
-    # Notify player of victory or defeat
-    if word_solved:
-        print('Congratulations! You won!')
-    else:
-        print('Try again next time!')
+    # TODO: implement the hangman game
 
     # Ask player if he/she wants to try again
     try_again = input('Would you like to try again? [y/Y] ')
