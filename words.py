1ab04355c0172682e9948847a01b073239d0ae64
Andrew Yang <ayang14@illinois.edu>
2017-04-19 11:51:42 -0500

    Use scalable get_word by default

--- words.py
+++ words.py
@@ -5,21 +5,14 @@ import random
 WORDLIST = 'wordlist.txt'
 
 
-def get_random_word():
-    """Get a random word from the wordlist."""
-    words = []
-    with open(WORDLIST, 'r') as f:
-        for word in f:
-            words.append(word)
-    return random.choice(words)
-
-
-def get_random_word_scalable():
+def get_random_word(min_word_length):
     """Get a random word from the wordlist using no extra memory."""
     num_words_processed = 0
     curr_word = None
     with open(WORDLIST, 'r') as f:
         for word in f:
+            if len(word) < min_word_length:
+                continue
             num_words_processed += 1
             if random.randint(1, num_words_processed) == 1:
                 curr_word = word
