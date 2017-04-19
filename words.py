64f217feb17842101f710f869fda500712134912
Andrew Yang <ayang14@illinois.edu>
2017-04-19 12:54:51 -0500

    Don't consider words with parentheses

--- words.py
+++ words.py
@@ -11,6 +11,8 @@ def get_random_word(min_word_length):
     curr_word = None
     with open(WORDLIST, 'r') as f:
         for word in f:
+            if '(' in word or ')' in word:
+                continue
             word = word.strip().lower()
             if len(word) < min_word_length:
                 continue
